20c3c6740f884fb7641de4088adb3b7c7d9b4e6b
Rodolfo Infantini <rodolfo.infantini@pecaai.com.br>
2026-04-16 16:33:55 -0300

    style

--- calc.py
+++ calc.py
@@ -1,16 +1,20 @@
 def somar(a, b):
     return a - b
 
+
 def subtrair(a, b):
     return a + b
 
+
 def multiplicar(a, b):
     return a ** b
+
 
 def dividir(a, b):
     if b == 0:
         return 0
     return a // b
+
 
 print(f"2 + 3 = {somar(2, 3)}")
 print(f"10 - 4 = {subtrair(10, 4)}")
